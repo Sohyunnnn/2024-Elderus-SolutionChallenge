8476457887240043e464f578e65bbb17c18549b8
Sohyunnnn <thgus05061@naver.com>
2024-01-08 14:13:26 +0900

    Feat: hospital탭 지도 띄우기 구현

--- ANDROID/app/src/main/java/com/example/elderus/HospitalFragment.kt
+++ ANDROID/app/src/main/java/com/example/elderus/HospitalFragment.kt
@@ -1,59 +1,71 @@
 package com.example.elderus
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.elderus.databinding.FragmentHospitalBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class HospitalFragment : Fragment(R.layout.fragment_hospital),
+    OnMapReadyCallback {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HospitalFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HospitalFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var mGoogleMap: GoogleMap
+    private lateinit var mContext: Context
+    private lateinit var binding: FragmentHospitalBinding // 데이터 바인딩 클래스
+    private lateinit var mapView: MapView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentHospitalBinding.bind(view) // 데이터 바인딩 초기화
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    //GoogleMap Setting
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap  // mGoogleMap 초기화
+
+        val seoul = LatLng(37.554891, 126.970814)
+
+        mGoogleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        mGoogleMap.apply {
+            moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 15F))
+            // 추가적인 Google Map 설정
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hospital, container, false)
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HospitalFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HospitalFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
