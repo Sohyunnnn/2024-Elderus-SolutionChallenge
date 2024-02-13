a9c8d828800187d23e8892be9e05146694577082
Sohyunnnn <thgus05061@naver.com>
2024-02-13 22:37:58 +0900

    Feat: 보호자 및 피보호자 회원가입 완료 페이지 뒤로가기 버튼 및 피보호자/보호자 추가 버튼 기능 구현

--- ANDROID/app/src/main/java/com/example/elderus/signUpGuardianCompleteActivity.kt
+++ ANDROID/app/src/main/java/com/example/elderus/signUpGuardianCompleteActivity.kt
@@ -1,11 +1,29 @@
 package com.example.elderus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatButton
 
 class signUpGuardianCompleteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_guardian_complete)
+
+
+        val AddWardButton = findViewById<AppCompatButton>(R.id.btn_add_ward)
+        AddWardButton.setOnClickListener {
+//            val intent = Intent(this, signUpOldCompleteActivity::class.java)
+//            startActivity(intent) // 다음 액티비티 시작
+//            finish() // 현재 액티비티 종료
+        }
+
+        val BackButton = findViewById<ImageView>(R.id.iv_complete_guardian_back)
+        BackButton.setOnClickListener {
+            val intent = Intent(this, SignUpGuardianActivity::class.java)
+            startActivity(intent) // 다음 액티비티 시작
+            finish() // 현재 액티비티 종료
+        }
     }
 }
--- ANDROID/app/src/main/java/com/example/elderus/signUpOldCompleteActivity.kt
+++ ANDROID/app/src/main/java/com/example/elderus/signUpOldCompleteActivity.kt
@@ -1,11 +1,30 @@
 package com.example.elderus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatButton
 
 class signUpOldCompleteActivity : AppCompatActivity() {
+
+    private lateinit var AddGuardianButton: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_old_complete)
+
+        AddGuardianButton = findViewById(R.id.btn_add_guardian)
+        AddGuardianButton.setOnClickListener {
+//            val intent = Intent(this, signUpOldCompleteActivity::class.java)
+//            startActivity(intent) // 다음 액티비티 시작
+//            finish() // 현재 액티비티 종료
+        }
+
+        val BackButton = findViewById<ImageView>(R.id.iv_complete_old_back)
+        BackButton.setOnClickListener {
+            val intent = Intent(this, SignUpOldActivity::class.java)
+            startActivity(intent) // 다음 액티비티 시작
+            finish() // 현재 액티비티 종료
+        }
     }
 }
